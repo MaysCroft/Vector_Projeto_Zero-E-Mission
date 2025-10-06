536b26e7a9bf9365000a1b670bcadf05c02b9ad9
May_Croft <mayconpilar19@gmail.com>
2025-10-06 20:29:52 -0300

    Vector Add New Window

---
+++ Vector_Zeroemission/ViewModel/Combustivel.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace Vector_Zeroemission.ViewModel;
+
+public partial class Combustivel : MetroWindow
+{
+    public Combustivel()
+    {
+        InitializeComponent();
+    }
+}

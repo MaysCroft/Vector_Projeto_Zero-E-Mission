fe9a29c957a7e30f34b6b77c85c203cdd676005e
May_Croft <mayconpilar19@gmail.com>
2025-10-07 19:20:19 -0300

    07/10/2025

--- Vector_Zeroemission/MainWindow.xaml.cs
+++ Vector_Zeroemission/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using Vector_Zeroemission.ViewModel;
 
 namespace Vector_Zeroemission;
 
@@ -20,5 +21,17 @@ public partial class MainWindow : MetroWindow
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Brioche_OnClick(object sender, RoutedEventArgs e)
+    {
+        Combustivel analise = new Combustivel();
+        analise.Show();
+
+        if (analise.IsLoaded)
+        {
+            Thread.Sleep(1000);
+            analise.Close();
+        }
     }
 }
